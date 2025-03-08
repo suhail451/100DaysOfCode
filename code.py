2853a81797ffed2e3d21ea6cc8e7ded0d463703c
Suhail Ahmed <arainsuhail451@gmail.com>
2025-03-08 21:14:49 +0500

    second problem solved

--- code.py
+++ code.py
@@ -1,3 +1,5 @@
+# problem one Finding the greatest Number in three
+
 one=int(input("entr the input one"))
 two=int(input("Enter the input two "))
 three=int(input("enter the third input"))
@@ -8,3 +10,11 @@ if  two >one and two>three:
     print(two,"is the greater") 
 if  three >one and three>two: 
     print(three,"is the greater")     
+
+
+#  problem two  Celcius to farenhight conversion
+print("celcius to farenhight converter")
+
+celcius=int(input("enter the celcius temprature"))
+farenhight=(celcius* 9/5)+32 
+print(celcius," is ",farenhight)
