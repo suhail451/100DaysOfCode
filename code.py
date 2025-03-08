6ad21a1acf33656cde52378c415b69c4c7a8e88f
Suhail Ahmed <arainsuhail451@gmail.com>
2025-03-08 20:12:03 +0500

    new first code

---
+++ code.py
@@ -0,0 +1,10 @@
+one=int(input("entr the input one"))
+two=int(input("Enter the input two "))
+three=int(input("enter the third input"))
+
+if one >two and one>three:
+    print(one,"is the greteast") 
+if  two >one and two>three: 
+    print(two,"is the greater") 
+if  three >one and three>two: 
+    print(three,"is the greater")     
