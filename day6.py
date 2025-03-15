d507b586055ac73b89a0490ee8c51442dc066927
Suhail Ahmed <arainsuhail451@gmail.com>
2025-03-16 00:00:11 +0500

    second problem added

--- day6.py
+++ day6.py
@@ -1,4 +1,5 @@
 #  Program to find simple intrest when parametes are given 
+import math as m
 
 principal_amout=int(input("Enter Principal Amount :"))
 rate_intrest=int(input("Enter Rate of Intrest :"))
@@ -9,3 +10,23 @@ def simple_intrest(x,y,z):
     print("Simple Intrest is ",simple_intrest)
 
 simple_intrest(principal_amout,rate_intrest,Time)
+
+
+#  Program to find the colume of the cylinder and cost of milk can be filled
+
+
+radius=int(input("Enter the radius of base :"))
+height=int(input("Enter the height of cylinder :"))
+
+def volume(a,b,c):
+    volume=a*(b**2)*c
+    print("volume of cylinder is ",volume)
+
+price=40
+def cost(v,price):
+    cost=volume * price
+    print("cost is",cost)
+
+volume(radius,m.pi,height)
+
+cost(volume,price)
