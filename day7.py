55bc2973d23ba2945bf39a40077f3b8863846431
Suhail Ahmed <arainsuhail451@gmail.com>
2025-04-13 12:24:35 +0500

    Division

---
+++ day7.py
@@ -0,0 +1,13 @@
+
+
+
+print("DIVISION")
+
+num=int(input("Enter the number :"))
+if(num % 6==0):
+        print("Yes given number is dividible by 6 and 3 ")
+elif(num %3==0):
+        print("The number is divisible by 3 but not 6")        
+else :
+        print("No this number is not divisible by 6 and nor 3 ") 
+print("Thank you ")       
